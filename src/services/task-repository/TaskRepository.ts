1cafca60798b6dd6ee8393093ed5e8d6deb93464
pablo <pabloluceroschneider@gmail.com>
2024-01-07 00:44:48 -0300

    Add insertTask

--- src/services/task-repository/TaskRepository.ts
+++ src/services/task-repository/TaskRepository.ts
@@ -7,7 +7,8 @@ type TasksByStatus = {
 }
 
 export class TaskRepository {
-	static API_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT76-g6vC3THuKZW43RYz1_LpQqvkwzSmgEQZcZNSmH5PHFfy2ctLydp7HQ7iXFmCTuiwSNkKksN1Ve/pub?output=tsv"
+	static URL_GSHEET = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT76-g6vC3THuKZW43RYz1_LpQqvkwzSmgEQZcZNSmH5PHFfy2ctLydp7HQ7iXFmCTuiwSNkKksN1Ve/pub?output=tsv";
+	static URL_GFORM = "https://docs.google.com/forms/d/e/1FAIpQLSdNGhqkdIo0_dHbEtD5ifg3rGp2bf4I4OfRGZfG1V-oSwRTCg/viewform?usp=pp_url&entry.222672610={ID}&entry.1294680739={TITLE}&entry.858858995={DESCRIPTION}&entry.369402124={ASSIGNED_TO}&entry.1460689905={STATUS}&entry.1172412426={PRIORITY}";
 
 	/**
 	 * Fetch tasks grouped by status
@@ -56,8 +57,25 @@ export class TaskRepository {
 	 */
 	static async fetch(): Promise<string> {
 		try {
-			const response = await fetch(TaskRepository.API_URL);
+			const response = await fetch(TaskRepository.URL_GSHEET);
 			return await response.text();
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	static async insertTask(task: Task) {
+		try {
+			const URL = TaskRepository.URL_GSHEET;
+			URL.replace("{ID}", String(task.id));
+			URL.replace("{TITLE}", task.title);
+			URL.replace("{DESCRIPTION}", task.description);
+			URL.replace("{ASSIGNED_TO}", task.assignedTo);
+			URL.replace("{STATUS}", task.status);
+			URL.replace("{PRIORITY}", task.priority);
+
+			const response = await fetch(URL);
+			return response;
 		} catch (error) {
 			throw error;
 		}
